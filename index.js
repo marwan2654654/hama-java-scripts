4cd169a00404fb177d82ef5e1edd19f2536bb98d
marwan2654654 <marwandz2009222@gmail.com>
2026-03-05 15:10:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,38 +13,76 @@ const files = {
 document.body.appendChild(newTarger);
 document.body.appendChild(newWindow);
 
-fetch(files.index).then(res => res.json()).then(data => CreatAdd(data));
-function CreatAdd(data){
-    if(!data || !data.SmartlinckSex || !data.smartLinckAll) {console.error('data not defined'); return}
+fetch(files.index)
+.then(res => res.json())
+.then(data => CreatAdd(data))
+.catch(err => console.error("Fetch Error:", err));
 
+function CreatAdd(data){
+
+    if(!data || !data.SmartlinckSex || !data.smartLinckAll){
+        console.error('Data not defined correctly');
+        return;
+    }
 
     smartLinckAll = data.smartLinckAll;
     smartLincksex = data.SmartlinckSex;
 
+    // وضع HTML فقط
     if(plaseAdd){
         plaseAdd.forEach(i => {
             i.innerHTML = data.babber;
         });
     }
 
-    if(NativeBanner) NativeBanner.innerHTML = data.NativeBanner;
+    if(NativeBanner){
+        NativeBanner.innerHTML = data.NativeBanner;
+    }
+
+    // تحميل سكربت الإعلان بعد إدخال العنصر
+    loadAdScript();
 }
 
-function getWindowSmartLinck(bool = true, target){
-    if(!smartLinckAll) return;
-    if(!smartLincksex) return;
+function loadAdScript(){
+
+    // إعدادات الشبكة الإعلانية
+    window.atOptions = {
+        'key' : 'bf82d08b4cfbca89197fb5aa8eab981e',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+    };
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://www.highperformanceformat.com/bf82d08b4cfbca89197fb5aa8eab981e/invoke.js";
+    script.async = true;
+
+    document.getElementById("ad-slot").appendChild(script);
+}
+
+function getWindowSmartLinck(target){
+
+    if(!smartLinckAll || !smartLincksex) return;
 
     newTarger.href = target;
     newTarger.target = "_blank";
     newTarger.style.display = "none";
 
     let randomChise = parseInt(Math.random() * 2) + 1;
-    if(randomChise === 1) newWindow.href = smartLinckAll;
-    if(randomChise === 2) newWindow.href = smartLincksex;
+
+    if(randomChise === 1){
+        newWindow.href = smartLinckAll;
+    }else{
+        newWindow.href = smartLincksex;
+    }
+
+    newWindow.target = "_blank";
     newWindow.style.display = "none";
 
     newTarger.click();
-    setTimeout(() =>newWindow.click(), 100);
+    setTimeout(() => newWindow.click(), 150);
 }
 
-alert('tis is oky')
+console.log("Ads Script Loaded Successfully");
